551ab61e98d54db59cd22d45c4eecb2139cd1ab3
Klinet <akali.klinet@gmail.com>
2025-04-02 12:18:15 +0200

    [dev] TemplateRenderer in index.php

--- public/index.php
+++ public/index.php
@@ -2,6 +2,22 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/../vendor/autoload.php';
+chdir(__DIR__ . '/..');
+require_once 'bootstrap.php';
 
-// Routing indulhat itt
+use App\Infrastructure\TemplateRenderer;
+
+$renderer = new TemplateRenderer();
+
+// Tesztadat
+$posts = [
+	[
+		'id' => 1,
+		'title' => 'Teszt bejegyzés',
+		'content' => 'Ez egy demó poszt tartalma...',
+		'author' => 'Admin',
+		'publish_at' => date('Y-m-d H:i'),
+	]
+];
+
+$renderer->render('guest/home.tpl', ['posts' => $posts]);
